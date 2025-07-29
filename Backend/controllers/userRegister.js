be5e47281715bd14e647937c155dd6674b6f9f7f
Surya <suryapugazh27@gmail.com>
2025-07-30 00:28:00 +0530

    Removed duplicate db connectin in Register page

--- Backend/controllers/userRegister.js
+++ Backend/controllers/userRegister.js
@@ -1,16 +1,6 @@
 const mysql = require("mysql2");
 const bcrypt = require("bcryptjs");
-
-const db = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-  timezone: "Z", // Set timezone to UTC
-});
+const db = require("../config/db");
 
 exports.register = async (req, res) => {
   const fname = req.body.fname;
