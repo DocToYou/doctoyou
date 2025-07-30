8e11860a144e63f79a8534850f3f5e3be6467e58
Surya <suryapugazh27@gmail.com>
2025-07-30 12:23:05 +0530

    Added routes for sendOtp and verifyOtp

--- Backend/routes/allRoutes.js
+++ Backend/routes/allRoutes.js
@@ -3,9 +3,12 @@ const router = express.Router();
 const userRegister = require("../controllers/userRegister");
 const userLogin = require("../controllers/userLogin");
 const homePage = require("../controllers/homePage");
+const otpVerification = require("../controllers/otpVerification");
 
 router.get("/", homePage.home);
 router.post("/register",userRegister.register);
 router.post("/login", userLogin.login);
+routet.post("/sendOtp", otpVerification.sendOtp);
+routet.post("/verifyOtp", otpVerification.verifyOtp);
 
 module.exports = router;
